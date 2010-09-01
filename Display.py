88a55d54e4cd22a5bfba32c3b11585bc717ca5ad
tps12 <tps12@columbia.edu>
2010-09-01 12:32:40 -0400

    Draw dirty values.

--- Display.py
+++ Display.py
@@ -9,9 +9,10 @@ class Display:
 
     def main_loop(self):
 
-        planet = Planet(200,1)
+        planet = Planet(200,1,(0,True))
         row = random.randint(1, planet.row_count-2)
-        planet.rows[row][random.randint(0, planet.row_lengths[row]-1)] = 255
+        planet.rows[row][random.randint(0,
+                                        planet.row_lengths[row]-1)] = (255,True)
 
         pygame.init()    
 
@@ -19,8 +20,7 @@ class Display:
                                          HWSURFACE)
         pygame.display.set_caption('Planet')
 
-        background = pygame.Surface(screen.get_size())
-        background.fill((128,128,128))
+        screen.fill((128,128,128))
 
         limit = pygame.time.Clock()
 
@@ -41,7 +41,7 @@ class Display:
             adjustments = []
             for row in range(planet.row_count):
                 for column in range(planet.row_lengths[row]):
-                    value = planet.rows[row][column]
+                    (value,dirty) = planet.rows[row][column]
                     if value > 10:
                         if column > 1:
                             adjustments.append((row,column-1,value-10))
@@ -49,20 +49,21 @@ class Display:
                             adjustments.append((row,column+1,value-10))
 
             for (row,column,value) in adjustments:
-                planet.rows[row][column] = value
+                planet.rows[row][column] = (value,True)
 
-            screen.blit(background, (0,0))
             screen.lock()
             for y in range(0, screen.get_height()):
                 for x in range(0, screen.get_width()):
                     if in_bounds(x,y):
-                        value = planet.rows[y][x - planet.row_offsets[y]]                                
-                        screen.set_at((x,y),(value,value,value))
+                        (value,dirty) = planet.rows[y][x - planet.row_offsets[y]]
+                        if dirty:
+                            screen.set_at((x,y),(value,value,value))
+                            planet.rows[y][x - planet.row_offsets[y]] = (value,False)
             screen.unlock()
             
             pygame.display.flip()
 
-            limit.tick(500)
+            limit.tick()
 
 if __name__ == '__main__':
     Display().main_loop()
