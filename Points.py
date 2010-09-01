5be671c6307ff283cfcc7a68114e682c65b18c05
tps12 <tps12@columbia.edu>
2010-09-01 19:05:31 -0400

    Point sprites.

--- Points.py
+++ Points.py
@@ -33,6 +33,16 @@ class Display:
 
         points = pygame.sprite.Group()
 
+        for n in range(10):
+            point = pygame.sprite.Sprite()
+            point.image = pygame.Surface((10,10))
+            pygame.draw.circle(point.image, (255,0,0), (5,5), 5)
+            row = random.randint(1, planet.row_count-1)
+            column = random.randint(0, planet.row_lengths[row]-1)
+            point.rect = pygame.Rect((column + planet.row_offsets[row], row),
+                                     point.image.get_size())
+            points.add(point)
+
         limit = pygame.time.Clock()
 
         done = False
