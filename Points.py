4626e29ebf4038a03aa1c40931033f2263f830dc
tps12 <tps12@columbia.edu>
2010-09-01 18:54:01 -0400

    Points display test.

---
+++ Points.py
@@ -0,0 +1,55 @@
+import random
+
+import pygame
+from pygame.locals import *
+
+from Planet import Planet
+
+class Display:
+
+    def main_loop(self):
+
+        planet = Planet(100,1,0)
+
+        pygame.init()    
+
+        screen = pygame.display.set_mode((planet.max_row,planet.row_count),
+                                         HWSURFACE)
+        pygame.display.set_caption('Planet')
+
+        screen.fill((128,128,128))
+
+        def in_bounds(x,y):
+            return (x > planet.row_offsets[y] and
+                    x < planet.row_offsets[y] + planet.row_lengths[y])
+
+        screen.lock()
+        for y in range(0, screen.get_height()):
+            for x in range(0, screen.get_width()):
+                if in_bounds(x,y):
+                    value = planet.rows[y][x - planet.row_offsets[y]]
+                    screen.set_at((x,y),(value,value,value))
+        screen.unlock()
+
+        points = pygame.sprite.Group()
+
+        limit = pygame.time.Clock()
+
+        done = False
+
+        while not done:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    done = True
+                elif event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        done = True
+
+            points.draw(screen)
+            
+            pygame.display.flip()
+
+            limit.tick()
+
+if __name__ == '__main__':
+    Display().main_loop()
