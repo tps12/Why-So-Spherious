8d91ea5ac719cc00a15ad7d46c5e37d498244111
tps12 <tps12@columbia.edu>
2010-09-01 21:19:19 -0400

    Points move vertically.

--- Planet.py
+++ Planet.py
@@ -12,9 +12,12 @@ class Planet:
         self.row_offsets = [int((self.max_row - row_length)/2.0 + 0.5)
                             for row_length in self.row_lengths]
 
+    def get_slope(self, row, nrow):
+        return float(self.row_lengths[int(nrow)])/self.row_lengths[int(row)]
+
     def get_adjacent_row(self, row, column, nrow):
         if nrow >= 0 and nrow < self.row_count - 1 and self.row_lengths[int(nrow)]:
-            m = float(self.row_lengths[int(nrow)])/self.row_lengths[int(row)]
+            m = self.get_slope(row, nrow)
             start = int(column * m)
             end = int((column + 1) * m) + 1
             if start < 0:
@@ -56,17 +59,26 @@ class Planet:
     def get_row_column(self, x, y, size=None):
         size = size or (0,0)
         row = y + size[1]/2
-        return row, x - self.row_offsets[row] + size[0]/2
+        return row, x - self.row_offsets[int(row)] + size[0]/2
 
     def apply_heading(self, v, theta, x, y, size=None):
         row, column = self.get_row_column(x, y, size)
+
+        # vertical component
+        vy = v * sin(theta)
+        nrow = row + vy
+        if nrow < 0:
+            nrow = -nrow
+        elif nrow >= self.row_count:
+            nrow = self.row_count - (nrow - self.row_count)
+        m = self.get_slope(row, nrow)
         
         # horizontal component
         vx = v * cos(theta)
         column += vx
         if column < 0:
-            column += self.row_lengths[int(row)]
-        elif column > self.row_lengths[int(row)] - 1:
-            column -= self.row_lengths[int(row)]
-        return self.get_coordinates(row, column, size)
+            column += self.row_lengths[int(nrow)]
+        elif column > self.row_lengths[int(nrow)] - 1:
+            column -= self.row_lengths[int(nrow)]
+        return self.get_coordinates(nrow, column, size)
 
