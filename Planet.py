4ace866df7e0662b3e814a34d513ca04b664efab
tps12 <tps12@columbia.edu>
2010-10-09 21:35:07 -0400

    Rotate about axis to move.

--- Planet.py
+++ Planet.py
@@ -9,6 +9,7 @@
 
 from math import *
 from numpy import *
+from numpy.linalg import *
 
 class Planet:
     def __init__(self, radius, tile_size, value):
@@ -176,8 +177,31 @@ class Planet:
         return (((self.bearing(lat2,lon2,lat,lon) + pi) % (2*pi),) +
                 self.get_coordinates_from_lat_lon(lat2, lon2, size))
 
+    def rotate(self, p, axis, theta):
+        # http://inside.mines.edu/~gmurray/ArbitraryAxisRotation/ArbitraryAxisRotation.html
+        L2 = sum([i*i for i in axis])
+        return array([(axis[0]*sum(p * axis) +
+                       (p[0]*(axis[1]*axis[1]+axis[2]*axis[2]) +
+                        axis[0]*(-axis[1]*p[1]-axis[2]*p[2])) * cos(theta) +
+                       sqrt(L2)*(-axis[2]*p[1]+axis[1]*p[2]) * sin(theta))/L2,
+                      (axis[1]*sum(p * axis) +
+                       (p[1]*(axis[0]*axis[0]+axis[2]*axis[2]) +
+                        axis[1]*(-axis[0]*p[0]-axis[2]*p[2])) * cos(theta) +
+                       sqrt(L2)*(axis[2]*p[0]-axis[0]*p[2]) * sin(theta))/L2,
+                      (axis[2]*sum(p * axis) +
+                       (p[2]*(axis[0]*axis[0]+axis[1]*axis[1]) +
+                        axis[2]*(-axis[0]*p[0]-axis[1]*p[1])) * cos(theta) +
+                       sqrt(L2)*(-axis[1]*p[0]+axis[0]*p[1]) * sin(theta))/L2])
+                       
+
     def apply_velocity(self, p, v):
-        return p, v
+        axis = cross(p, v)
+        axis = axis / norm(axis)
+        
+        p_prime = self.rotate(p, axis, 0.01)
+        
+        
+        return p_prime, v
 
     def apply_heading(self, v, theta, x, y, size=None):
         row, column = self.get_row_column(x, y, size)
